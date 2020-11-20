bddfcc4d96324b55677aed06eacde0f15cb146db
uoytha <06689@farnborugh.ac.uk>
2020-11-20 10:28:54 +0000

    Optimised den2Bin function

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(den2Bin(12));
-        System.out.println(bin2Den("00001101"));
+        System.out.println(bin2Den("11111111"));
     }
 
     public static String den2Bin(int denary) {
-        int[] bits = {64, 32, 16, 8, 4, 2, 1};
+        int[] bits = {128, 64, 32, 16, 8, 4, 2, 1};
         String binary = "";
-        for (int i = 0; i < bits.length; i++) {
+        for (int i = 0; i < 8; i++) {
             if (denary >= bits[i]) {
-                if ((denary - bits[i]) >= 0) {
-                    binary += "1";
-                    denary -= bits[i];
-                } else {
-                    binary += "0";
-                }
+                binary += "1";
+                denary -= Math.pow(2, 7 - i);
             } else {
                 binary += "0";
             }
@@ -28,12 +24,17 @@ public class Main {
         int denary = 0;
         String bit = "";
         for (int i = 0; i < binary.length(); i++) {
-            bit = binary.substring(i, i+1);
+            bit = binary.substring(i, i + 1);
             if (bit.equals("1")) {
                 denary += Math.pow(2, binary.length() - i - 1);
             }
         }
         return denary;
+    }
+
+    public static String hex2Bin(String binary) {
+        String hex = "";
+        return hex;
     }
 
 }
